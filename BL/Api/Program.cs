f1b9fcf9af6e6e33c6b289b4d7636121fbdb807d
Dmitry <borushko.dmitry@gmail.com>
2025-06-17 16:58:00 +0300

    initial commit

---
+++ BL/Api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.OpenApi.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(o =>
+        {
+            o.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        });
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                opt.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.Run();
+    }
+}
