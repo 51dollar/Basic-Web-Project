e8adc5329bf775ed4f39bdb28517aa348dfe6917
Dmitry <borushko.dmitry@gmail.com>
2025-07-10 22:06:21 +0300

    Add register user with roles

--- BL/Api/Data/ApplicationDBContext.cs
+++ BL/Api/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 using Api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +15,27 @@ namespace Api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "8f38d225-f9ea-4cf5-9d79-5d7e9cf1770a",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "f6b7c55f-9c35-4dd5-8eab-f7cf96225849",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
