ebbe5bda42ca19d9642296c86d6d6428de3d703b
Dmitry <borushko.dmitry@gmail.com>
2025-07-14 23:38:35 +0300

    Add Portfolio HttpDelete request

--- BL/Api/Repository/PortfolioRepository.cs
+++ BL/Api/Repository/PortfolioRepository.cs
@@ -21,6 +21,24 @@ namespace Api.Repository
             return portfolio;
         }
 
+        public async Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol)
+        {
+            var portfolioModel = await _dBContext.Portfolios
+            .FirstOrDefaultAsync(x =>
+                x.AppUserId == appUser.Id &&
+                x.Stock.Symbol.ToLower() == symbol.ToLower()
+            );
+
+            if (portfolioModel == null)
+            {
+                return null;
+            }
+
+            _dBContext.Portfolios.Remove(portfolioModel);
+            await _dBContext.SaveChangesAsync();
+            return portfolioModel;
+        }
+
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             return await _dBContext.Portfolios
