b0bc741894fce26f7110b0fd1ad362499b75de7f
51dollar <borushkoddima@mail.ru>
2025-09-14 15:21:47 +0300

    add AuthService

--- UI/frontend/src/App.tsx
+++ UI/frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from "./Components/Navbar/Navbar.tsx";
 import {Outlet} from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import {ToastContainer} from "react-toastify";
 
 function App() {
     return (
         <>
-            <Navbar/>
-            <Outlet/>
+            <Navbar />
+            <Outlet />
+            <ToastContainer />
         </>
     );
 }
---
+++ UI/frontend/src/Services/AuthService.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {handleError} from "../Helpers/ErrorHandler.tsx";
+import type {UserProfileToken} from "../Models/User.ts";
+
+const api = "http://localhost:5000/api"
+
+export const loginAPI = async (username: string, password: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + "account/login", {
+            username: username,
+            password: password,
+        });
+        return data
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const registerAPI = async (username: string, email: string, password: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + "account/register", {
+            username: username,
+            email: email,
+            password: password,
+        });
+        return data
+    } catch (error) {
+        handleError(error);
+    }
+};
