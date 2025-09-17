ed57810d8d6c8a16e21368f2e56215156ddd3712
51dollar <borushkoddima@mail.ru>
2025-09-17 18:38:21 +0300

    fix name

--- UI/frontend/src/App.tsx
+++ UI/frontend/src/App.tsx
@@ -3,16 +3,16 @@ import Navbar from "./Components/Navbar/Navbar.tsx";
 import {Outlet} from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import {ToastContainer} from "react-toastify";
-import {UserProvader} from "./Context/useAuth.tsx";
+import {UserProvider} from "./Context/useAuth.tsx";
 
 function App() {
     return (
         <>
-            <UserProvader>
+            <UserProvider>
                 <Navbar />
                 <Outlet />
                 <ToastContainer />
-            </UserProvader>
+            </UserProvider>
         </>
     );
 }
